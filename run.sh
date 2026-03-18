9e5e69d86294f293e410df73f8bb357529444282
Niwelk <ivan.chervenkov11@gmail.com>
2026-03-19 01:17:14 +0300

    simplified script file

--- run.sh
+++ run.sh
@@ -2,17 +2,8 @@
 
 source /home/niwelk/venv/bin/activate
 
-echo "1) Запуск FastAPI main.py"
-python -m uvicorn main:app --reload --host 0.0.0.0 --port 8000 &
-BACKEND_PID=$!
+uvicorn main:app --reload --port 8000 &
 
-echo "2) Запуск монитора ресурсов monitoring.py"
-python monitoring.py &
-MONITOR_PID=$!
-
-echo "3) Запуск streamlit интерфейса app.py"
 streamlit run app.py --server.headless=true &
-FRONTEND_PID=$!
 
-trap "kill $BACKEND_PID $MONITOR_PID $FRONTEND_PID" INT TERM
 wait
